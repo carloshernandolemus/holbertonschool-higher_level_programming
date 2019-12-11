22b23b6d27ac66b017569a76391583675eb58143
chmlsud <1188@holbertonschool.com>
2019-12-11 20:06:54 +0000

    Fix the ord function and confitional

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    result = ''
-    for char in c:
-        if ord(char) >= 65:
-            result += chr(ord(char) - 32)
-    return result
+    if ord(c) >= 97 and ord(c) <=122:
+        return(True)
+    else:
+        return(False)
