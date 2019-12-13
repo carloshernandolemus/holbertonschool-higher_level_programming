3fcee17d2a447ed1dc130040495f457fe40e511c
chmlsud <1188@holbertonschool.com>
2019-12-13 02:09:11 +0000

    Function that prints all the names defined by the compiled module

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sys import argv
+import hidden_4
+
+def main():
+
+    for var1 in dir(hidden_4):
+        if var1[0] != '_' and var1[1] != '_':
+            print(var1)
+
+if __name__ == "__main__":
+    main()
