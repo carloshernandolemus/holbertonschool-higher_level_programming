dd8941276133f54d33fa37402e0b13d46f2ae12f
chmlsud <1188@holbertonschool.com>
2019-12-15 21:26:12 +0000

    Add the sys module to print with write function

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+
+def main():
+
+     sys.stdout.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+     exit(1)
+
+__name__ == "__main__"
+main()
