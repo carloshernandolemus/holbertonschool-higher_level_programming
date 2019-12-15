b27baa50c839abc3f230438cd62d37e085bde1dc
chmlsud <1188@holbertonschool.com>
2019-12-15 21:42:39 +0000

    Fix the script stderr

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 import sys
 
-
-def main():
-
-     text = "and that piece of art is useful - Dora Korpar, 2015-10-19"
-     sys.stdout.write("{}\n".format(text))
-     exit(1)
-
-__name__ == "__main__"
-main()
+text = "and that piece of art is useful - Dora Korpar, 2015-10-19"
+sys.stderr.write("{}\n".format(text))
+exit(1)
