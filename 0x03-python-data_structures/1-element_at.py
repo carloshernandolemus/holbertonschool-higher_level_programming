3f3a17875ffae51a8a936b06925fbc507c3e9623
chmlsud <1188@holbertonschool.com>
2019-12-17 18:45:59 +0000

    Fix the conditional from and to or

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0 and idx > my_list:
-        return(none)
+    if idx < 0 or idx > len(my_list):
+        return(None)
     else:
-        for x in my_list:
-            return(my_list[idx])
+        return(my_list[idx])
