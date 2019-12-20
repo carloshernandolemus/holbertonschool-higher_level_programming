b04ecf38a6cb623654ee71a12b7a659aa2bb90ac
chmlsud <1188@holbertonschool.com>
2019-12-20 21:17:49 +0000

    Fix the return

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,4 +4,4 @@ def best_score(a_dictionary):
         return (None)
     my_list = sorted(a_dictionary.items(), key=lambda py: (py[1], py[0]))
     p, y = my_list[-1]
-    return (k)
+    return (p)
