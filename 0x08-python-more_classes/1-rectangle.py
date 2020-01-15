6303eb683eed7007ecd9ef331d83ec7aaeb5b8e0
chmlsud <1188@holbertonschool.com>
2020-01-15 18:02:01 +0000

    Fix the spaces pep8

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,13 +4,16 @@
 class Rectangle:
 
     def __init__(self, width=0, height=0):
+
         self.width = width
         self.height = height
 
     def width(self):
+
         return self.__width
 
     def width(self, value):
+
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -18,9 +21,11 @@ class Rectangle:
         self.__width = value
 
     def height(self):
+
         return self.__height
 
     def height(self, value):
+
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
