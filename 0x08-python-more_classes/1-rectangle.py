21369dbd9b6f0386b3ab4547863ca5af81eb6ecd
chmlsud <1188@holbertonschool.com>
2020-01-15 18:10:24 +0000

    Fix the pep8

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,10 +8,12 @@ class Rectangle:
         self.width = width
         self.height = height
 
+    @property
     def width(self):
 
         return self.__width
 
+    @width.setter
     def width(self, value):
 
         if not isinstance(value, int):
@@ -20,10 +22,12 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         self.__width = value
 
+    @property
     def height(self):
 
         return self.__height
 
+    @height.setter
     def height(self, value):
 
         if not isinstance(value, int):
