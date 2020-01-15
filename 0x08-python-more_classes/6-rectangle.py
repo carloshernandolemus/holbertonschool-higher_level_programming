ce465017f08ec693a1eed2f72a923420bd12de8b
chmlsud <1188@holbertonschool.com>
2020-01-15 20:08:35 +0000

    Fix to add numbers of the instances

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -3,10 +3,13 @@
 
 class Rectangle:
 
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
 
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
