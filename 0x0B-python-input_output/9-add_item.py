f11d8328b3ae6040cd1d2b73021a4aa335eb6e67
chmlsud <1188@holbertonschool.com>
2020-01-22 15:52:11 +0000

    Creates a new function with the fix code

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
+
 import sys
-import os.path
+import os
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+args = sys.argv
 
+if not os.path.exists('add_item.json'):
+    with open('add_item.json', mode='w', encoding='utf-8') as f:
+        f.write('[]')
 
-save_file = __import__('7-save_to_json_file').save_to_json_file
-load_file = __import__('8-load_from_json_file').load_from_json_file
+obj = load_from_json_file('add_item.json')
 
-my_list = []
-if os.path.exists("add_item.json"):
-    my_list = load_file("add_item.json")
+for arg in args[1:]:
+    obj.append(arg)
 
-for arg in sys.argv[1:]:
-    my_list.append(arg)
-
-save_file(my_list, "add_item.json")
+save_to_json_file(obj, 'add_item.json')
