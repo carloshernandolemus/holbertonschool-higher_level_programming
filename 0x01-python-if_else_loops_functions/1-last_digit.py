c8317e25037845347228a27605a0b856cbb104e6
chmlsud <1188@holbertonschool.com>
2020-04-28 20:01:34 +0000

    Fix the conditionals

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-module = number % 10
-modulen = number % -10
-a = "Last digit of"
-b = "and is less that 6 and not 0"
-c = "and is greater than 5"
-d = "and is 0"
-
-if number > 1 and module < 6 and module != 0:
-    print("{:s} {:d} is {:d} {:s}".format(a, number, module, b))
-if number < -1 and modulen < 6 and modulen != 0:
-    print("{:s} {:d} is {:d} {:s}".format(a, number, modulen, b))
-if number > 1 and module > 5:
-    print("{:s} {:d} is {:d} {:s}".format(a, number, module, c))
-if number > 1 and module == 0:
-    print("{:s} {:d} is {:d} {:s}".format(a, number, module, d))
-if number < -1 and modulen == 0:
-    print("{:s} {:d} is {:d} {:s}".format(a, number, module, d))
+if number >= 0:
+    module = number % 10
+else:
+    module = number % -10
+print("Last digit of {} is {} ".format(number, module), end="")
+if module > 5:
+    print("and is greater than 5")
+elif module == 0:
+    print("and is 0")
+else:
+    print("and is less than 6 and not 0")
