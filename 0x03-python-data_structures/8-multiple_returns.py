2aa061a73523405ad5e238508a7d3f3434be2366
chmlsud <1188@holbertonschool.com>
2020-05-06 00:37:03 +0000

    Change the variable to return value

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        var1 = None
+        return(None)
     else:
         var1 = sentence[0]
     return(len(sentence), var1)
