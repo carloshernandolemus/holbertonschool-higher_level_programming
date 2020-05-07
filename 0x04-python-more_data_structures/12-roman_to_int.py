cbff1049dc4b6e00b07009f0cd2b5cdd9fb0521e
chmlsud <1188@holbertonschool.com>
2020-05-07 23:43:23 +0000

    Fix the variables

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string or not isinstance(roman_string, str):
-        return (0)
-    var1 = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-    var2 = roman_string
-    romlist = [var1[i[0]] if var1[i[0]] >= var1[i[1]] else (-1*var1[i[0]])
-             for i in zip(var2, var2[1:] + var2[-1])]
-    var3 = sum(romlist)
-    return (var3)
+    if roman_string is None or type(roman_string) != str:
+        return(0)
+
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    var1 = 0
+
+    for x in range(0, len(roman_string)):
+        if x > 0 and roman[roman_string[x]] > roman[roman_string[x - 1]]:
+            var1 += roman[roman_string[x]] - roman[roman_string[x - 1]] * 2
+        else:
+            var1 += roman[roman_string[x]]
+    return (var1)
