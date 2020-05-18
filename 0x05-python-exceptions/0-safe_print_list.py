00cb3f78eb4480580c0c6bd04bbafc4bded1ff1b
chmlsud <1188@holbertonschool.com>
2020-05-18 22:16:18 +0000

    Add he new varibale

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    var2 = 0
     for var1 in range(x):
         try:
             print("{}".format(my_list[var1]), end="")
-            var1 += 1
+            var2 += 1
         except:
             exit
     print()
-    return(var1)
+    return(var2)
