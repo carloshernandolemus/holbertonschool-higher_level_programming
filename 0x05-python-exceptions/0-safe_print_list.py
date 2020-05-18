bd0cf9b6e4c2f2c48ca2549a72bc947fa3890859
chmlsud <1188@holbertonschool.com>
2020-05-18 20:02:46 +0000

    Erase the variable

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    var1 = 0
-    for var2 in range(x):
+    for var1 in range(x):
         try:
-            print("{}".format(my_list[var2]), end="")
+            print("{}".format(my_list[var1]), end="")
             var1 += 1
         except:
-            break
+            exit
     print()
-    return var1
+    return(var1)
