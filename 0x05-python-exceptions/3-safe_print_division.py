49cc6c87024b3d2758627977eb5c053d197f95f6
chmlsud <1188@holbertonschool.com>
2020-05-18 22:52:38 +0000

    Add the finally clause

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,5 +4,6 @@ def safe_print_division(a, b):
         var1 = a / b
     except:
         var1 = None
-    print("Inside result: {}".format(var1))
-    return(var1)
+    finally:
+        print("Inside result: {}".format(var1))
+        return(var1)
