5b87273e62b1ff2e60692656db3f910fb69b5441
chmlsud <1188@holbertonschool.com>
2020-05-19 22:03:12 +0000

    Add the property setter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""This is the squere class"""
+
+
+class Square:
+    """This part define the size"""
+
+    def __init__(self, size=0):
+        """Init the size"""
+        self.size = size
+
+    @property
+    def size(self):
+        """Size of the square"""
+        return(self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Set the size"""
+        if type(value) != int:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        """Return the area of the square"""
+        return(self.__size * self.__size)
