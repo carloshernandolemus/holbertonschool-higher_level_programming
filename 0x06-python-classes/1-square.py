32818cdadff84e155cffce73398f522aa1af7806
chmlsud <1188@holbertonschool.com>
2020-05-19 21:41:30 +0000

    Add the size private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This is the squere class"""
+
+
+class Square:
+    pass
+
+"""This is the private attributte size"""
+
+    def __init__(self, size=0):
+        self.__size = size
