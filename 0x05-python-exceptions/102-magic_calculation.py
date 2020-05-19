a97d45bcce02ab1512c229dfcb9fd3d70d087705
chmlsud <1188@holbertonschool.com>
2020-05-19 02:27:07 +0000

    Fix the variable

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    e = 0
+    equal = 0
     for var1 in range(1, 3):
         try:
             if (var1 > a):
                 raise Exception('Too far')
             else:
-                e += a ** b / var1
+                equal += a ** b / var1
         except:
-            e = b + a
+            equal = b + a
             break
     return(equal)
