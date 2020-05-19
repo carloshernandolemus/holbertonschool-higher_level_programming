c7d3d5f8d458db02b4b71658b3c068acd777d73b
chmlsud <1188@holbertonschool.com>
2020-05-19 22:08:34 +0000

    Fix the spaces

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -11,17 +11,17 @@ class Square:
     @property
     def size(self):
         """Size of the square"""
-        return(self.__size)
+        return self.__size
 
     @size.setter
     def size(self, value):
         """Set the size"""
         if type(value) is not int:
             raise TypeError('size must be an integer')
-        if value < 0:
+        if (value < 0):
             raise ValueError('size must be >= 0')
         self.__size = size
 
     def area(self):
         """Return the area of the square"""
-        return(self.__size * self.__size)
+        return self.__size * self.__size
