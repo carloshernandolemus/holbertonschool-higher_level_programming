b3c8635ae7b528ac38b0603252e17bcc4a8ebbb3
chmlsud <1188@holbertonschool.com>
2020-05-19 22:10:19 +0000

    Fix the conditionals

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""This is the squere class"""
+"""This is the square class"""
 
 
 class Square:
-    """This part define the size"""
+    """A class that define the size"""
     def __init__(self, size=0):
-        """Init the size"""
+        """Initialize the size"""
         self.size = size
 
     @property
     def size(self):
-        """Size of the square"""
+        """Get the size of the square"""
         return self.__size
 
     @size.setter
@@ -20,8 +20,8 @@ class Square:
             raise TypeError('size must be an integer')
         if (value < 0):
             raise ValueError('size must be >= 0')
-        self.__size = size
+        self.__size = value
 
     def area(self):
         """Return the area of the square"""
-        return self.__size * self.__size
+        return (self.__size * self.__size)
