526127af79acdfa27728ba5a02d2a237c023bcc0
chmlsud <1188@holbertonschool.com>
2020-05-27 14:58:00 +0000

    Add the locked class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -5,8 +5,5 @@ This is a module that containts a class
 
 
 class LockedClass:
-    __slots__ = ['first_name']
-
-    def __init__(self):
-        """ Init method """
-        pass
+    """Class Locked"""
+    __slots__ = 'first_name'
