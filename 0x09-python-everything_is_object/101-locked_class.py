853a67613f908ba4953013039dfadba61a93c768
chmlsud <1188@holbertonschool.com>
2020-05-27 05:04:04 +0000

    Fix the comments

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+"""
+This is a module that containts a class
+"""
 
 
 class LockedClass:
     __slots__ = ['first_name']
 
     def __init__(self):
+        """ Init method """
         pass
