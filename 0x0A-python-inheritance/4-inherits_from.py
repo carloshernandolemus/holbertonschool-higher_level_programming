2f73876ffdee0040159fb0d854600f787aff3e7b
chmlsud <1188@holbertonschool.com>
2020-06-03 02:12:40 +0000

    Create a function that returns True if the object is an instance of a class that inherited (directly or indirectly) from the specified class ; otherwise False

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Def the function prototype"""
+
+
+
+def inherits_from(obj, a_class):
+    """Conditional class"""
+    if type(obj) is a_class:
+        return(False)
+    return(isinstance(obj, a_class))
