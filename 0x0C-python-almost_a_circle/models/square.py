c36edecfbbc05eef987c1e0778f8e808aa3e4941
chmlsud <1188@holbertonschool.com>
2020-06-08 18:36:40 +0000

    Update the class Square by adding the public getter and setter size

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+from models.rectangle import Rectangle
+""" import module """
+
+
+class Square(Rectangle):
+    """ class that inherits from Rectangle """
+    def __init__(self, size, x=0, y=0, id=None):
+        """ constructor """
+        super().__init__(size, size, x, y, id)
+        """ attribute access """
