82b2e9fae716a5e86caa167fb87cf43c06e48344
chmlsud <1188@holbertonschool.com>
2020-09-23 04:51:45 +0000

    Fix the url

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
-const req = require('request');
-const pnt = process.argv[2];
-const zelda = `https://swapi-api.hbtn.io/api/films/${pnt}/`;
-req.get(zelda, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(JSON.parse(body).title);
-  }
+const request = require('request');
+
+const url = 'https://swapi-api.hbtn.io/api/films/';
+request(url + process.argv[2], function (error, response, body) {
+  console.log(error || JSON.parse(body).title);
 });
